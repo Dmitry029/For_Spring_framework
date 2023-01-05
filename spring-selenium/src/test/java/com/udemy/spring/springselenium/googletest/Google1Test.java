c23dd382d3dfcf4c3a5646cd41e58aa67c02e052
Dzmitry Kastjuchenka <Dzmitry_Kastjuchenka@epam.com>
2023-01-05 23:41:45 +0300

    delete unused imports

--- spring-selenium/src/test/java/com/udemy/spring/springselenium/googletest/Google1Test.java
+++ spring-selenium/src/test/java/com/udemy/spring/springselenium/googletest/Google1Test.java
@@ -1,15 +1,14 @@
 package com.udemy.spring.springselenium.googletest;
 
 import com.udemy.spring.springselenium.SpringBaseTestNGTest;
-import com.udemy.spring.springselenium.page.google.GooglePage;
 import com.udemy.spring.springselenium.kelvin.service.ScreenShotService;
+import com.udemy.spring.springselenium.page.google.GooglePage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertTrue;
 
 public class Google1Test extends SpringBaseTestNGTest {
     @Autowired
--- spring-selenium/src/test/java/com/udemy/spring/springselenium/googletest/Google2Test.java
+++ spring-selenium/src/test/java/com/udemy/spring/springselenium/googletest/Google2Test.java
@@ -2,15 +2,13 @@ package com.udemy.spring.springselenium.googletest;
 
 import com.udemy.spring.springselenium.SpringBaseTestNGTest;
 import com.udemy.spring.springselenium.kelvin.annotation.LazyAutowired;
-import com.udemy.spring.springselenium.page.google.GooglePage;
 import com.udemy.spring.springselenium.kelvin.service.ScreenShotService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.Assert;
+import com.udemy.spring.springselenium.page.google.GooglePage;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertTrue;
 
 public class Google2Test extends SpringBaseTestNGTest {
     @LazyAutowired
