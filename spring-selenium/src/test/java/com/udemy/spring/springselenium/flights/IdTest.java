4a616f7163c9f595989a426b44a6d9df16bc662e
Dzmitry Kastjuchenka <Dzmitry_Kastjuchenka@epam.com>
2023-01-05 23:14:26 +0300

    correct gitignore

--- spring-selenium/src/test/java/com/udemy/spring/springselenium/flights/IdTest.java
+++
@@ -1,7 +0,0 @@
-package com.udemy.spring.springselenium.flights;
-
-import org.springframework.test.context.TestPropertySource;
-
-@TestPropertySource(properties = "app.locale=id")
-public class IdTest extends FlightsTest{
-}
