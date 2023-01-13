b384ad0e748562f9494534478127a3c5b87c5555
Dzmitry Kastjuchenka <Dzmitry_Kastjuchenka@epam.com>
2023-01-13 23:08:17 +0300

    add window switch test

---
+++ spring-selenium/src/main/java/com/udemy/spring/springselenium/kelvin/annotation/Window.java
@@ -0,0 +1,15 @@
+package com.udemy.spring.springselenium.kelvin.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Page
+@Documented
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Window {
+    String value() default "";
+}
