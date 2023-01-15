28996194e91676a06b11e37f5812c85951d7a3e7
Dzmitry Kastjuchenka <Dzmitry_Kastjuchenka@epam.com>
2023-01-15 23:26:49 +0300

    add 1 test

--- spring-selenium/src/test/java/com/udemy/spring/springselenium/googletest/Google1Test.java
+++ spring-selenium/src/test/java/com/udemy/spring/springselenium/googletest/Google1Test.java
@@ -1,2 +1,28 @@
-package com.udemy.spring.springselenium.googletest;public class Google1Test {
+package com.udemy.spring.springselenium.googletest;
+
+import com.udemy.spring.springselenium.SpringBaseTestNGTest;
+import com.udemy.spring.springselenium.kelvin.annotation.LazyAutowired;
+import com.udemy.spring.springselenium.kelvin.service.ScreenShotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Google1Test extends SpringBaseTestNGTest {
+    @LazyAutowired
+    private GooglePage googlePage;
+
+    @LazyAutowired
+    private ScreenShotService screenShotService;
+
+    @Test
+    public void googleTest() throws IOException {
+        this.googlePage.goTo();
+        assertTrue(this.googlePage.isAt());
+        this.googlePage.getSearchComponent().search("spring boot");
+        assertTrue(this.googlePage.getSearchResult().isAt());
+        assertTrue(this.googlePage.getSearchResult().getCount() > 2);
+        screenShotService.takeScreenshot();
+        this.googlePage.close();
+    }
 }
