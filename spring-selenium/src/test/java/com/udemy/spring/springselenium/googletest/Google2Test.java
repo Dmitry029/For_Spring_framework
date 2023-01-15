c2b563fc354da8303aa32f3083539c287511b073
Dzmitry Kastjuchenka <Dzmitry_Kastjuchenka@epam.com>
2023-01-15 23:42:46 +0300

    add

--- spring-selenium/src/test/java/com/udemy/spring/springselenium/googletest/Google2Test.java
+++ spring-selenium/src/test/java/com/udemy/spring/springselenium/googletest/Google2Test.java
@@ -4,14 +4,13 @@ import com.udemy.spring.springselenium.SpringBaseTestNGTest;
 import com.udemy.spring.springselenium.kelvin.annotation.LazyAutowired;
 import com.udemy.spring.springselenium.kelvin.service.ScreenShotService;
 import com.udemy.spring.springselenium.page.google.GooglePage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 import static org.testng.Assert.assertTrue;
 
-public class Google1Test extends SpringBaseTestNGTest {
+public class Google2Test extends SpringBaseTestNGTest {
     @LazyAutowired
     private GooglePage googlePage;
 
@@ -22,14 +21,10 @@ public class Google1Test extends SpringBaseTestNGTest {
     public void googleTest() throws IOException {
         this.googlePage.goTo();
         assertTrue(this.googlePage.isAt());
-        this.googlePage.getSearchComponent().search("spring boot");
-
-        googlePage.getSearchResult().is
-
-        assertTrue(this.googlePage.getSearchResult().//.isAt());
-        assertTrue(this.googlePage.getSearchResult().//getCount() > 2);
-
-                screenShotService.takeScreenshot();
+        this.googlePage.getSearchComponent().search("selenium");
+        assertTrue(this.googlePage.getSearchResult().isAt());
+        assertTrue(this.googlePage.getSearchResult().getCount() > 2);
+        screenShotService.takeScreenshot();
         this.googlePage.close();
     }
 }
