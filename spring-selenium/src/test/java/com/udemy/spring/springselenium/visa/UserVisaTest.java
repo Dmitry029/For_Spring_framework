a6045c85f53be3f470d357a050188a255162a3ce
Dzmitry Kastjuchenka <Dzmitry_Kastjuchenka@epam.com>
2023-01-16 21:29:04 +0300

    test gets data from xml

--- spring-selenium/src/test/java/com/udemy/spring/springselenium/visa/UserVisaTest.java
+++ spring-selenium/src/test/java/com/udemy/spring/springselenium/visa/UserVisaTest.java
@@ -13,6 +13,8 @@ import java.sql.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.sql.Date.valueOf;
+
 public class UserVisaTest extends SpringBaseTestNGTest {
 
     @Autowired
@@ -41,14 +43,35 @@ public class UserVisaTest extends SpringBaseTestNGTest {
                     this.registrationPage.getConfirmationNumber()
             );
         });
-
     }
 
-    /*@Autowired
-    private UserRepository repository;*/
+    @Test
+    public void visa2Test() {
+        List<Customer> users =
+                this.repository
+                        .findByDobBetween(valueOf("1995-01-01"),
+                                valueOf("1999-01-01"))
+                .stream()
+                .limit(3)
+                .collect(Collectors.toList());
 
-    /*@Test(dataProvider = "getData")
-    public void visaTest(Customer u){
+        users.forEach(u -> {
+            this.registrationPage.goTo();
+            this.registrationPage.setNames(u.getFirstName(), u.getLastName());
+            this.registrationPage.setCountryFromAndTo(u.getFromCountry(), u.getToCountry());
+            this.registrationPage.setBirthDate(u.getDob().toLocalDate());
+            this.registrationPage.setContactDetails(u.getEmail(), u.getPhone());
+            this.registrationPage.setComments(u.getComments());
+            this.registrationPage.submit();
+
+            System.out.println("Confirmation Number: " +
+                    this.registrationPage.getConfirmationNumber()
+            );
+        });
+    }
+
+    @Test(dataProvider = "getData")
+    public void visa3Test(Customer u){
         this.registrationPage.goTo();
         this.registrationPage.setNames(u.getFirstName(), u.getLastName());
         this.registrationPage.setCountryFromAndTo(u.getFromCountry(), u.getToCountry());
@@ -60,17 +83,20 @@ public class UserVisaTest extends SpringBaseTestNGTest {
         logger.info("Request confirmation # INFO : " + this.registrationPage.getConfirmationNumber());
         logger.warn("Request confirmation # WARN : " + this.registrationPage.getConfirmationNumber());
 
-    }*/
+    }
 
-    /*@DataProvider
+    /**
+     * Get test param from testng-suite.xml
+     */
+    @DataProvider
     public Object[][] getData(ITestContext context){
         return this.repository.findByDobBetween(
-                        Date.valueOf("2000-01-01"),
-                        Date.valueOf("2010-01-01")
+                        valueOf(context.getCurrentXmlTest().getParameter("dobFrom")),
+                        valueOf(context.getCurrentXmlTest().getParameter("dobTo"))
                 )
                 .stream()
                 .limit(3)
                 .map(o -> new Customer[]{o})
                 .toArray(Object[][]::new);
-    }*/
+    }
 }
