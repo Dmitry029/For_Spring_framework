7cf040a51350418096386fbc66e3499e876633ec
Dzmitry Kastjuchenka <Dzmitry_Kastjuchenka@epam.com>
2023-01-19 19:30:18 +0300

    rename methods

--- spring-selenium/src/test/java/com/udemy/spring/springselenium/bdd/VisaSteps.java
+++ spring-selenium/src/test/java/com/udemy/spring/springselenium/bdd/VisaSteps.java
@@ -1,40 +1,67 @@
 package com.udemy.spring.springselenium.bdd;
 
+import com.udemy.spring.springselenium.kelvin.annotation.LazyAutowired;
+import com.udemy.spring.springselenium.page.visa.VisaRegistrationPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StringUtils;
 
+import java.time.LocalDate;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+@SpringBootTest
+@CucumberContextConfiguration
 public class VisaSteps {
+
+    @LazyAutowired
+    private VisaRegistrationPage registrationPage;
+
     @Given("I am on VISA registartion form")
-    public void iAmOnVISARegistartionForm() {
+    public void launchSite() {
+        registrationPage.goTo();
+        assertTrue(registrationPage.isAt());
     }
 
     @When("I select my from country {string} and to country {string}")
-    public void iSelectMyFromCountryAndToCountry(String arg0, String arg1) {
+    public void selectCountry(String from, String to) {
+        registrationPage.setCountryFromAndTo(from, to);
     }
 
     @And("I enter my dob as {string}")
-    public void iEnterMyDobAs(String arg0) {
+    public void enterDob(String dob) {
+        registrationPage.setBirthDate(LocalDate.parse(dob));
     }
 
     @And("I enter my name as {string} and {string}")
-    public void iEnterMyNameAsAnd(String arg0, String arg1) {
+    public void enterName(String fn, String ln) {
+        registrationPage.setNames(fn, ln);
     }
 
     @And("I enter my contact details as {string} and {string}")
-    public void iEnterMyContactDetailsAsAnd(String arg0, String arg1) {
+    public void enterContactDetails(String email, String phone) {
+        registrationPage.setContactDetails(email, phone);
     }
 
     @And("I enter my comment {string}")
-    public void iEnterMyComment(String arg0) {
+    public void enterComment(String comment) {
+        registrationPage.setComments(comment);
     }
 
     @And("I submit the form")
-    public void iSubmitTheForm() {
+    public void submit() {
+        registrationPage.submit();
     }
 
     @Then("I should see get the confirmation number")
-    public void iShouldSeeGetTheConfirmationNumber() {
+    public void verifyConfirmationNumber() {
+        boolean isEmpty =
+                StringUtils.isEmpty(this.registrationPage.getConfirmationNumber().trim());
+        assertFalse(isEmpty);
     }
 }
